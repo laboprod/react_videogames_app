27644031fc2dd4d2f06252d926f0788c5e0e8c16
laboprod <labeau.francois@gmail.com>
2021-01-14 12:21:20 +0100

    Game detail styling and react router

--- src/actions/detailAction.js
+++ src/actions/detailAction.js
@@ -3,6 +3,10 @@ import { gameDetailsURL } from '../api';
 import { gameScreenshotURL } from '../api';
 
 export const loadDetail = (id) => async (dispatch) => {
+	dispatch({
+		type: 'LOADING_DETAIL',
+	});
+
 	const detailData = await axios.get(gameDetailsURL(id));
 	const screenShotData = await axios.get(gameScreenshotURL(id));
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,14 @@ import GameDetail from '../components/GameDetail';
 // Styling and Animation
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
+	// get the current location
+	const location = useLocation();
+	const pathId = location.pathname.split('/')[2];
+	console.log(pathId);
+
 	// FETCH GAMES
 	const dispatch = useDispatch();
 	useEffect(() => {
@@ -19,6 +25,7 @@ const Home = () => {
 
 	return (
 		<GameList>
+			{pathId && <GameDetail />}
 			<h2>Upcoming Games</h2>
 			<Games>
 				{upcoming.map((game) => (
