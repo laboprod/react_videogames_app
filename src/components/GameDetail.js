2ecc35900dcf534015a0c364de76541e2970c1ab
laboprod <labeau.francois@gmail.com>
2021-01-18 12:22:47 +0100

    Components transition

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -7,7 +7,7 @@ import { smallImage } from '../util';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-const GameDetail = () => {
+const GameDetail = ({ pathId }) => {
 	const history = useHistory();
 	// Exit Detail
 	const exitDetailHandler = (e) => {
@@ -24,10 +24,10 @@ const GameDetail = () => {
 		<>
 			{!isLoading && (
 				<CardShadow className='shadow' onClick={exitDetailHandler}>
-					<Detail>
+					<Detail layoutId={pathId}>
 						<Stats>
 							<div className='rating'>
-								<h3>{game.name}</h3>
+								<motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
 								<p>Rating: {game.rating}</p>
 							</div>
 							<Info>
@@ -40,7 +40,7 @@ const GameDetail = () => {
 							</Info>
 						</Stats>
 						<Media>
-							<img src={smallImage(game.background_image, 1280)} alt={game.background_image} />
+							<motion.img layoutId={`image ${pathId}`} src={smallImage(game.background_image, 1280)} alt={game.background_image} />
 						</Media>
 						<Description>
 							<p>{game.description_raw}</p>
@@ -63,6 +63,7 @@ const CardShadow = styled(motion.div)`
 	overflow-y: scroll;
 	background: rgba(0, 0, 0, 0.5);
 	position: fixed;
+	z-index: 10;
 	top: 0;
 	left: 0;
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,13 @@ import Game from '../components/Game';
 import GameDetail from '../components/GameDetail';
 // Styling and Animation
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 import { useLocation } from 'react-router-dom';
 
 const Home = () => {
 	// get the current location
 	const location = useLocation();
 	const pathId = location.pathname.split('/')[2];
-	console.log(pathId);
 
 	// FETCH GAMES
 	const dispatch = useDispatch();
@@ -25,25 +24,27 @@ const Home = () => {
 
 	return (
 		<GameList>
-			{pathId && <GameDetail />}
-			<h2>Upcoming Games</h2>
-			<Games>
-				{upcoming.map((game) => (
-					<Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
-				))}
-			</Games>
-			<h2>Popular Games</h2>
-			<Games>
-				{popular.map((game) => (
-					<Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
-				))}
-			</Games>
-			<h2>New Games</h2>
-			<Games>
-				{newGames.map((game) => (
-					<Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
-				))}
-			</Games>
+			<AnimateSharedLayout type='crossfade'>
+				<AnimatePresence>{pathId && <GameDetail pathId={pathId} />}</AnimatePresence>
+				<h2>Upcoming Games</h2>
+				<Games>
+					{upcoming.map((game) => (
+						<Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
+					))}
+				</Games>
+				<h2>Popular Games</h2>
+				<Games>
+					{popular.map((game) => (
+						<Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
+					))}
+				</Games>
+				<h2>New Games</h2>
+				<Games>
+					{newGames.map((game) => (
+						<Game name={game.name} released={game.released} id={game.id} image={game.background_image} key={game.id} />
+					))}
+				</Games>
+			</AnimateSharedLayout>
 		</GameList>
 	);
 };
