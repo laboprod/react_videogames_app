c3114b27eb51004231753725854784b46fd6f04a
laboprod <labeau.francois@gmail.com>
2021-01-18 13:02:24 +0100

    Rendering stars and platform icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,6 +6,16 @@ import { smallImage } from '../util';
 // REDUX
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+// IMAGES
+import playstation from '../img/playstation.svg';
+import steam from '../img/steam.svg';
+import xbox from '../img/xbox.svg';
+import nintendo from '../img/nintendo.svg';
+import apple from '../img/apple.svg';
+import gamepad from '../img/gamepad.svg';
+// STARS
+import starEmpty from '../img/star-empty.png';
+import starFull from '../img/star-full.png';
 
 const GameDetail = ({ pathId }) => {
 	const history = useHistory();
@@ -15,6 +25,44 @@ const GameDetail = ({ pathId }) => {
 		if (element.classList.contains('shadow')) {
 			document.body.style.overflow = 'auto';
 			history.push('/');
+		}
+	};
+
+	// GET STARS
+	const getStars = () => {
+		const stars = [];
+		const rating = Math.round(game.rating);
+		for (let i = 1; i <= 5; i++) {
+			if (i <= rating) {
+				stars.push(<img alt='star' key={i} src={starFull}></img>);
+			} else {
+				stars.push(<img alt='star' key={i} src={starEmpty}></img>);
+			}
+		}
+		return stars;
+	};
+
+	// GET PLATFORM IMAGES
+	const getPlatform = (platform) => {
+		switch (platform) {
+			case 'PlayStation 4':
+				return playstation;
+			case 'PlayStation 5':
+				return playstation;
+			case 'Xbox One':
+				return xbox;
+			case 'Xbox Series S/X':
+				return xbox;
+			case 'Xbox S':
+				return xbox;
+			case 'PC':
+				return steam;
+			case 'Nintendo Switch':
+				return nintendo;
+			case 'iOS':
+				return apple;
+			default:
+				return gamepad;
 		}
 	};
 
@@ -29,12 +77,13 @@ const GameDetail = ({ pathId }) => {
 							<div className='rating'>
 								<motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
 								<p>Rating: {game.rating}</p>
+								{getStars()}
 							</div>
 							<Info>
 								<h3>Platforms</h3>
 								<Platforms>
 									{game.platforms.map((data) => (
-										<h3 key={data.platform.id}>{data.platform.name}</h3>
+										<img key={data.platform.id} src={getPlatform(data.platform.name)} alt={data.platform.name}></img>
 									))}
 								</Platforms>
 							</Info>
@@ -98,6 +147,12 @@ const Stats = styled(motion.div)`
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
+
+	img {
+		width: 1.5rem;
+		height: 1.5rem;
+		display: inline-block;
+	}
 `;
 
 const Info = styled(motion.div)`
