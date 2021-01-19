a1fe12d065a88eaa2bf171531e899cbda6a277ce
laboprod <labeau.francois@gmail.com>
2021-01-19 12:12:35 +0100

    Add search bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 // Components and pages
 import Home from './pages/Home';
 import GlobalStyles from './components/GlobalStyles';
+import Nav from './components/Nav';
+// Router
 import { Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='App'>
 			<GlobalStyles />
+			<Nav />
 			<Route path={['/game/:id', '/']}>
 				<Home />
 			</Route>
