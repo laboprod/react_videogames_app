bbf62c288d3373ba0d973a5254e10a70c34e4e60
laboprod <labeau.francois@gmail.com>
2021-01-19 12:48:08 +0100

    Navbar search and reset on click logo

--- src/api.js
+++ src/api.js
@@ -44,3 +44,6 @@ export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
 
 // GAME Screenshot
 export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots`;
+
+// Search Game
+export const searchGameURL = (game_name) => `${base_url}games?search=${game_name}&page_size=9`;
--- src/util.js
+++ src/util.js
@@ -1,5 +1,6 @@
 // MEDIA RESIZE
 export const smallImage = (imagePath, size) => {
+	if (!imagePath) return null;
 	const image = imagePath.match(/media\/screenshots/)
 		? imagePath.replace('media/screenshots', `media/resize/${size}/-/screenshots`)
 		: imagePath.replace('/media/games/', `/media/resize/${size}/-/games/`);
